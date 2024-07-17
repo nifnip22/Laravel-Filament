01cfae2a466edc06a0f12e1cf07fe983badf3534
nifnip22 <proxigaming742@gmail.com>
2024-07-17 21:36:41 +0800

    Make a Filament Relation Managers for Treatment CRUD

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -89,7 +89,7 @@ class PatientResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\TreatmentsRelationManager::class,
         ];
     }
 
